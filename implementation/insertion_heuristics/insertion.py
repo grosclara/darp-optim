20cc4bb138a3ef87c239111ef99f4a4359386baa
Clara <clara.gros@student-cs.fr>
2020-04-02 12:32:18 +0200

    new insertion algo

--- implementation/insertion_heuristics/insertion.py
+++ implementation/insertion_heuristics/insertion.py
@@ -1,216 +1,210 @@
-# -*- coding: utf-8 -*-
+import json
+import pandas
+#extraction des donnes
+with open("week_data.json") as json_data:
+    data = json.load(json_data)
 
-from heapq import heappush, heappop
-from logging import log, DEBUG
-from math import inf
-from operator import itemgetter
+time_data = pandas.read_csv("travel_times.csv", sep=';')
 
-from data import sets, node_to_station, parameters, nb_bookings, bookings, shifts
-from class_init import ShiftScheduleBlock, Insertion
+nb_stations = len(time_data)
+time = {}
 
+for i in range(nb_stations):
+    for j in range(nb_stations):
+        time["s"+str(i), "s"+str(j)] = int(time_data["s{}".format(i)][j])
+#creation de dictionnaire pour utiliser les données plus facilement (avoir accès à toutes les données uniquement avec les id)
+station={}
+prix={}
+charge={}
+duree={}
+clients={}
+debut={}
+fin={}
 
-def _new_potential_insertions(shift, booking):
-    _add_into_same_block(shift = shift, booking = booking)
-    _add_into_different_blocks(shift = shift, booking = booking) # Merge blocks
+depose={}
+prise={}
+dureemax={}
 
-def _add_into_same_block(shift, booking):
-    """
-        Trying to fit the booking into the existing working schedule while
-        pickup and delivery happen in the same block
-    """
-    # shift.feasibleSchedules = []
+capacite={}
+salairemax={}
 
-    if shift.currentSchedules == [] :
-        # case0 : first work block is being created if possible
-        _case0(shift = shift, booking = booking)
-    else :
-        # Case1 : pickup and delivery are inserted at the end of the working schedule.
-        _case1(shift = shift, booking = booking)        
-        # Case 2 : pickup and delivery are consecutive stops in a block
-        _case2(shift = shift, booking = booking)        
-        # Case 3 : pickup in the last block, delivery becomes last stop in schedule
-        _case3(shift = shift, booking = booking)        
-        # Case 4 : pickup and delivery are separated by at least one stop
-        _case4(shift = shift, booking = booking)
-
-
-def _add_into_different_blocks(shift, booking):
-    """
-        Pick up is at the end of a work block while delivery is at the start of the next one
-        D-1 possible insertions where D is the number of work blocks
-    """
-    pass
-
-def _case0(shift, booking):
-    """
-        First insertion to the shift schedule
-    """
-    pass
-
-def _case1(shift, booking):
-    """
-        Both the pickup and the delivery of booking are inserted at the end of the last work block, 
-        so that the result would be those two stops would turn into the last two of the last work block
-        Number of possible insertions = 1(at the end of the block only).
-    """
-
-    pass
-
-def _case2(shift, booking):
-    """
-        Both the pick-up and the delivery of booking are inserted between two consecutive stops on the schedule-block
-        Number of possible insertions = D*N(d) (D is the number of blocks and N(d) is the number of stops within the block d)
-    """
-    pass
-
-def _case3(shift, booking):
-    """
-        The pick-up of booking takes place somewhere within the last work block, while its delivery is inserted at the end of the work-schedule
-        Number of possible insertions = N
-    """
-
-    pass
-
-def _case4(shift, booking):
-    """
-        The pick-up and delivery of booking are separated by at least one other stop and the delivery is not the last stop on the 
-        expanded work schedule.
-        Number of possible insertions = D*N(d)*(N(d)-1)/2 (D is the number of blocks and N(d) is the number of stops within the block d)
-    """
-    pass
+for client in data["bookings"]:
+    dureemax[client["id"]]=client["maximumDuration"]
+    for job in client["jobs"]:
+        station[job["id"]]=job["station"]
+        duree[job["id"]]=job["duration"]
+        clients[job["id"]]=client["id"]
+        debut[job["id"]]=job["timeWindowBeginDate"]
+        fin[job["id"]]=job["timeWindowEndDate"]
+        if job["type"]=="PickUpJob":
+            prix[job["id"]]=client["price"]
+            charge[job["id"]]=client["passengers"]
+            prise[client["id"]]=job["id"]
+        else:
+            prix[job["id"]]=0
+            charge[job["id"]]=-client["passengers"]
+            depose[client["id"]]=job["id"]
+for voiture in data["shifts"]:
+    capacite[voiture["id"]]=voiture["capacity"]
+    salairemax[voiture["id"]]=voiture["maximumTurnover"]
+    prise[voiture["id"]]=voiture["jobs"][0]["id"]
+    depose[voiture["id"]]=voiture["jobs"][1]["id"]
+    for job in voiture["jobs"]:
+        debut[job["id"]]=voiture["jobs"][0]["timeDate"]
+        fin[job["id"]]=voiture["jobs"][1]["timeDate"]
+        duree[job["id"]]=0
+        station[job["id"]]="s0"
 
 
-def _update_capacity(schedule, insertion) :
-    pick_up_node = insertion.node_before_pick_up.next
-    drop_off_node = insertion.node_before_drop_off.next
-    node = pick_up_node
-
-    while node != drop_off_node : # Before the drop off node, increment the used capacity of each node
-        node["Used capacity"] += insertion.booking.passengers
-        node = node.next
-
-def _update_turnover(schedule, insertion) :
-    schedule.turnover += insertion.booking.passengers
-
-def _update_schedule_timing(schedule, insertion):
-    pass
-
-def _find_best_shift_schedule(shift, booking) :
-    pass
-
-def _set_current_schedule():
-    pass
 
 
-############### INSERTION HEURISTICS ###############
+#notation utilisée
+#voiture correspond à l'id associé à chaaque voiture'
+#course correspond à l'ensemble des course qu'une voiture verifie
 
 
-def insertion_init(parameters = parameters, bookings = bookings, shifts = shifts):
-            
-    # Build an ordered priority queue of potential bookings to initialize a route
-    # Bookings are sorted according to their earliest pick up time windows
-    seed_bookings = bookings.sort(reverse = True)
-    bookings_not_inserted = []
+def verifie(course,voiture):
+    #renvoie true si un ensemble de course est realisable par une voiture, false sinon
+    for k in range(1, len(course) - 1):
+        if  course[k][1]<debut[course[k][0]] or course[k][1]>fin[course[k][0]] :
+            #verifie les contraintes de borne de temps pour chaque job
+            return(False)
+        for i in range(k+1, len(course) - 1):
+            if course[i][0]==depose[clients[course[k][0]]]:
+                if course[i][1]-course[k][1]>dureemax[clients[course[k][0]]]:
+                    return(False)
+                    #verifie les contraintes de durée maximum des clients
+    salaire=0
+    charg=0
+    chargemax=0
+    for k in range(1, len(course) - 1):
+            salaire+=prix[course[k][0]]
+            charg+=charge[course[k][0]]
+            if charg>chargemax:
+                chargemax=charg
+    if chargemax>capacite[voiture] or salaire>salairemax[voiture]:
+        return(False)
+        #verifie les contraintes de charge et salaire max pour la voiture
+    for x in pause(course):
+        if x<0:
+            return(False)
+            #verifie que les trajets sont fait dans des temps réalisables
+    return(True)
 
-    # Process each booking 
-    # While there are unprocessed bookings :
-    while len(seed_bookings)>0 :
+def cout(course):
+    #calcule le cout lié à un ensemble de course (un seul vehicule)
+    cout=0
+    for k in range(len(course) - 1):
+        cout+=time[(station[course[k][0]], station[course[k + 1][0]])]
+    return(cout)
 
-        booking_to_schedule = seed_bookings.pop()
-
-        best_schedules = []
-
-        # Generate the potential insertions of booking_to_schedule for each shift
-        for shift in shifts :
-            _new_potential_insertions(shift = shift, booking = booking_to_schedule)
-            # Find the optimal schedule among each feasible schedule of the current shift
-            best_schedule = _find_best_shift_schedule(shift = shift, booking = booking_to_schedule)
-
-            if best_schedule != [inf]:
-                best_schedule.append(shift)
-                best_schedules.append(best_schedule)
-            
-        # Determine best overall schedule
-        if best_schedules != []:
-                best_schedules.sort()
-                schedule = best_schedules[0]
-                _set_current_schedule()
-            else:
-                bookings_not_inserted.append(booking_to_schedule)
-        
-        seed_bookings.remove(booking_to_schedule) 
-
-            
-# ---------------------------------------------------------------------
-def get_si_algorithm():
-    algo_name = "Insertion heuristic"
-    algo_desc = "Mole & Jameson (1976) sequential cheapest insertion heuristic "+\
-                "without local search (van Breedam 1994, 2002)"
-    return (algo_name, algo_desc, insertion_init(parameters, bookings, shifts, node_to_station))
-    
-if __name__=="__main__":
-    get_si_algorithm()
-    pass
+def pause(course):
+    #renvoie la liste des temps de pause intermediare pour un ensemble de course. Listepause[k] est la pause entre le keme et le k+1eme element de course
+    listepause=[]
+    for k in range(len(course) - 1):
+        listepause.append(course[k + 1][1] - time[(station[course[k][0]], station[course[k + 1][0]])] - duree[course[k][0]] - course[k][1])
+    return(listepause)
 
 
-############### CONSTRAINTS ###############
+def insertclient(client,voiture):
+    #renvoie la liste de toutes les ensemble de courses possible en inserant client dans  voiture
+    listeprecedentinsertion=[[],[]]
+    i=0
+    for job in [prise,depose]:
+        for k in range(1,len(res[voiture])):
+            #print(res[voiture][k][1])
+            if res[voiture][k][1]>=debut[job[client]]:
+                listeprecedentinsertion[i].append(k-1)
+            if res[voiture][k][1]>fin[job[client]]:
+                break
+        i+=1
+    listecandidat=[]
 
-def _check_capacity_constraint(schedule, booking, node_before_pick_up, node_before_drop_off):
-        
-    # If pick up and drop off are consecutive nodes
-    if node_before_pick_up == node_before_drop_off :
-        used_capacity = node_before_pick_up.value["Used capacity"] + booking.passengers
-        return used_capacity <= schedule.shift.capacity
+    for i in listeprecedentinsertion[0]:
+        for j in listeprecedentinsertion[1]:
+            candidat=[]
+            for x in res[voiture]:
+                candidat.append(x[:])
 
-    # If pick up and drop off node are separated by at least one station
-    else :
-        # Check shift capacity between pick up and drop off nodes
-        node = node_before_pick_up # Initialize checking at node before pick up
-        while node != node_before_drop_off : # Terminate checking at node before drop off
-            used_capacity = node.value["Used capacity"] + booking.passengers
-            if not used_capacity <= schedule.shift.capacity :
-                return False
-            node = node.next
-
-    return True
-
-
-def _check_turnover_constraint(schedule, booking):
-
-    return schedule.turnover + booking.price <= schedule.shift.max_turnover
-
-def _check_ride_time_constraint():
-#def _check_ride_time_constraint(schedule, deviation):
-    #Déviation entre deux stations ???
-
-    #Il faut séparer 3 types de shift
-    #ceux où le drop off est avant--> aucun décalage
-    #ceux où le pick up est avant et le drop off entre les deux > ride time + deviation pick-up
-    #ceux où le pick up est après -> aucun décalage
-    #pour calculer le ride time actuel on utilse AT_drop_off - AT_pick-up
-
-    return True
+            listepause=pause(candidat)
+            retard=time[(station[candidat[i][0]],station[prise[client]])]+time[(station[prise[client]],station[candidat[i+1][0]])]-time[(station[candidat[i][0]],station[candidat[i+1][0]])]+duree[prise[client]]
+            candidat=candidat[:i+1]+[[prise[client],max(debut[prise[client]],candidat[i][1]+time[(station[candidat[i][0]],station[prise[client]])]+duree[candidat[i][0]])]]+candidat[i+1:]
 
 
-############### UTILS ###############
+            for k in range(i+2,len(candidat)-1):
+                #print(retard,listepause[k-1])
+                retard=max(0,retard-listepause[k-1])
 
-def _compute_deviation(travel_time, node_before_pickup, node_before_drop_off, booking, schedule):
+                candidat[k][1]+=retard
 
-    # Compute also execution duration time !!
+            listepause=pause(candidat)
+            retard=time[(station[candidat[j+1][0]],station[depose[client]])]+time[(station[depose[client]],station[candidat[j+2][0]])]-time[(station[candidat[j+1][0]],station[candidat[j+2][0]])]+duree[depose[client]]
 
-    if node_before_drop_off == node_before_pickup :
-        deviation = - travel_time[node_before_pickup.value["Job"].station, node_before_pickup.next.value["Job"].station] \
-                    + travel_time[node_before_pickup.value["Job"].station, booking.jobs[0].station]\
-                    + travel_time[booking.jobs[0].station, booking.jobs[1].station]\
-                    + travel_time[booking.jobs[1].station, node_before_drop_off.next.value["Job"].station]
-            
-    else :
-        deviation_pick_up =  - travel_time[node_to_station[node_before_pickup.value], node_to_station[node_before_pickup.next.value]]\
-                            + travel_time[node_to_station[node_before_pickup.value], node_to_station[booking.jobs[0].job_id]] \
-                            + travel_time[node_to_station[booking.jobs[0].job_id]], node_to_station[node_before_pickup.next.value]
-        deviation_drop_off =  - travel_time[node_to_station[node_before_drop_off.value], node_to_station[node_before_drop_off.next.value]]\
-                            + travel_time[node_to_station[node_before_drop_off.value], node_to_station[booking.jobs[1].job_id]] \
-                            + travel_time[node_to_station[booking.jobs[1].job_id], node_to_station[node_before_drop_off.next.value]]
-        deviation = deviation_pick_up + deviation_drop_off
+            candidat=candidat[:j+2]+[[depose[client],max(debut[depose[client]],candidat[j+1][1]+time[(station[candidat[j+1][0]],station[depose[client]])]+duree[candidat[j+1][0]])]]+candidat[j+2:]
 
-    return deviation
+            for k in range(j+3,len(candidat)-1):
+                #print(retard,listepause[k-1])
+                retard=max(0,retard-listepause[k-1])
+
+                candidat[k][1]+=retard
+
+            if verifie(candidat,voiture):
+                listecandidat.append([cout(candidat),candidat])
+    return(listecandidat)
+
+def insertclientoptimal(client,voiture):
+    #trouve la façon optimal d'inserer  client dans voiture
+    listecandidat=insertclient(client,voiture)
+
+    if len(listecandidat)>0:
+        best_course=min(listecandidat)
+        return(best_course)
+    else:
+        return None
+
+# on insere les clients par ordre croissant d'arrivée
+res={}
+for voiture in capacite.keys():
+    res[voiture]=[[prise[voiture],debut[prise[voiture]]],[depose[voiture],fin[depose[voiture]]]]
+
+listeclient=[]
+clientrejete=[]
+
+for client in dureemax.keys():
+    listeclient.append((debut[depose[client]],client))
+listeclient.sort()
+
+for client in listeclient:
+    listecandidat=[]
+    for voiture in salairemax.keys():
+        course=insertclientoptimal(client[1], voiture)
+        if course!=None:
+            listecandidat.append([course[0] - cout(res[voiture]), course[1], voiture])
+    if len(listecandidat)>0:
+        best=min(listecandidat)
+        res[best[2]]=best[1]
+    else:
+        l=clientrejete.append(client)
+
+
+#ecriture du json
+nb_assigned_bookings=len(listeclient)-len(clientrejete)
+shifts=[]
+route_cost=0
+
+for voiture in res.keys():
+    route_cost+=cout(res[voiture])
+    dic={}
+    dic["id"]=voiture
+    jobs=[]
+    for job in res[voiture]:
+        jobs.append({"id":job[0],"time":job[1]})
+    dic["jobs"]=jobs
+    shifts.append(dic)
+
+
+resjson={"nb_assigned_bookings":nb_assigned_bookings,"route_cost":route_cost,"shifts":shifts}
+with open('results_week_insertion.json', 'w', encoding='utf-8') as f:
+    json.dump(resjson, f, indent=4)
+
+
