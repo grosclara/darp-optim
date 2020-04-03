f652bf93c638326ecc581f95453f84c475567529
Clara <clara.gros@student-cs.fr>
2020-04-03 16:17:26 +0200

    clean python code

--- implementation/insertion_heuristics/insertion.py
+++ implementation/insertion_heuristics/insertion.py
@@ -1,212 +1,253 @@
 import json
 import pandas
 
-# Extraction des données
+############# Data extraction #############
 with open("data/week_data.json") as json_data:
     data = json.load(json_data)
 
 time_data = pandas.read_csv("data/travel_times.csv", sep=';')
 
 nb_stations = len(time_data)
-time = {}
 
+time = {}
 for i in range(nb_stations):
     for j in range(nb_stations):
         time["s"+str(i), "s"+str(j)] = int(time_data["s{}".format(j)][i])
 
-#creation de dictionnaire pour utiliser les données plus facilement (avoir accès à toutes les données uniquement avec les id)
-station={}
-prix={}
-charge={}
-duree={}
-clients={}
-debut={}
-fin={}
+station = {}
+price = {}
+load_dict = {}
+duration = {}
+clients = {}
+start = {}
+end = {}
 
-depose={}
-prise={}
-dureemax={}
+pick_up = {}
+drop_off = {}
+max_ride_time = {}
 
-capacite={}
-salairemax={}
+capacity = {}
+max_turnover = {}
 
 for client in data["bookings"]:
-    dureemax[client["id"]]=client["maximumDuration"]
-    for job in client["jobs"]:
-        station[job["id"]]=job["station"]
-        duree[job["id"]]=job["duration"]
-        clients[job["id"]]=client["id"]
-        debut[job["id"]]=job["timeWindowBeginDate"]
-        fin[job["id"]]=job["timeWindowEndDate"]
-        if job["type"]=="PickUpJob":
-            prix[job["id"]]=client["price"]
-            charge[job["id"]]=client["passengers"]
-            prise[client["id"]]=job["id"]
-        else:
-            prix[job["id"]]=0
-            charge[job["id"]]=-client["passengers"]
-            depose[client["id"]]=job["id"]
-for voiture in data["shifts"]:
-    capacite[voiture["id"]]=voiture["capacity"]
-    salairemax[voiture["id"]]=voiture["maximumTurnover"]
-    prise[voiture["id"]]=voiture["jobs"][0]["id"]
-    depose[voiture["id"]]=voiture["jobs"][1]["id"]
-    for job in voiture["jobs"]:
-        debut[job["id"]]=voiture["jobs"][0]["timeDate"]
-        fin[job["id"]]=voiture["jobs"][1]["timeDate"]
-        duree[job["id"]]=0
-        station[job["id"]]="s0"
+
+    max_ride_time[client["id"]] = client["maximumDuration"]
+
+    for job in client["jobs"] :
+        station[job["id"]] = job["station"]
+        duration[job["id"]] = job["duration"]
+        clients[job["id"]] = client["id"]
+        start[job["id"]] = job["timeWindowBeginDate"]
+        end[job["id"]] = job["timeWindowEndDate"]
+
+        if job["type"] == "PickUpJob" :
+            price[job["id"]] = client["price"]
+            load_dict[job["id"]] = client["passengers"]
+            pick_up[client["id"]] = job["id"]
+
+        else :
+            price[job["id"]] = 0
+            load_dict[job["id"]] = -client["passengers"]
+            drop_off[client["id"]] = job["id"]
+
+for shift in data["shifts"] :
+
+    capacity[shift["id"]] = shift["capacity"]
+    max_turnover[shift["id"]] = shift["maximumTurnover"]
+    pick_up[shift["id"]] = shift["jobs"][0]["id"]
+    drop_off[shift["id"]] = shift["jobs"][1]["id"]
+
+    for job in shift["jobs"] :
+        start[job["id"]] = shift["jobs"][0]["timeDate"]
+        end[job["id"]] = shift["jobs"][1]["timeDate"]
+        duration[job["id"]] = 0
+        station[job["id"]] = "s0"
 
 
+def check_constraints(course, shift) :
+    """
+        Return True if a set of bookings is feasible by a shift, False otherwise
+    """
+    for k in range(1, len(course)-1) :
 
+        # Check time window constraint
+        if course[k][1] < start[course[k][0]] or course[k][1] > end[course[k][0]] :
+            return False
 
-#notation utilisée
-#voiture correspond à l'id associé à chaaque voiture'
-#course correspond à l'ensemble des course qu'une voiture verifie
+        # Check maximum ride time constraint
+        for i in range(k+1, len(course)-1) :
+            if course[i][0] == drop_off[clients[course[k][0]]]:
+                if course[i][1] - course[k][1] > max_ride_time[clients[course[k][0]]]:
+                    return False
 
+    # Check max load and turnover constraintes for shift
+    turnover = 0
+    load = 0
+    max_load = 0
+    for k in range(1, len(course)-1) :
+            turnover += price[course[k][0]]
+            load += load_dict[course[k][0]]
+            if load > max_load :
+                max_load = load
 
-def verifie(course,voiture):
-    #renvoie true si un ensemble de course est realisable par une voiture, false sinon
-    for k in range(1, len(course) - 1):
-        if  course[k][1]<debut[course[k][0]] or course[k][1]>fin[course[k][0]] :
-            #verifie les contraintes de borne de temps pour chaque job
-            return(False)
-        for i in range(k+1, len(course) - 1):
-            if course[i][0]==depose[clients[course[k][0]]]:
-                if course[i][1]-course[k][1]>dureemax[clients[course[k][0]]]:
-                    return(False)
-                    #verifie les contraintes de durée maximum des clients
-    salaire=0
-    charg=0
-    chargemax=0
-    for k in range(1, len(course) - 1):
-            salaire+=prix[course[k][0]]
-            charg+=charge[course[k][0]]
-            if charg>chargemax:
-                chargemax=charg
-    if chargemax>capacite[voiture] or salaire>salairemax[voiture]:
-        return(False)
-        #verifie les contraintes de charge et salaire max pour la voiture
-    for x in pause(course):
-        if x<0:
-            return(False)
-            #verifie que les trajets sont fait dans des temps réalisables
-    return(True)
+    if max_load > capacity[shift] or turnover > max_turnover[shift] :
+        return False
 
-def cout(course):
-    #calcule le cout lié à un ensemble de course (un seul vehicule)
-    cout=0
+    # Check that rides are done in a feasible time
+    for x in compute_slack_periods(course) :
+        if x < 0 :
+            return False
+
+    return True
+
+def cost(course):
+    """
+        Compute the cost of a set of bookings
+    """
+    cost = 0
     for k in range(len(course) - 1):
-        cout+=time[(station[course[k][0]], station[course[k + 1][0]])]
-    return(cout)
+        cost += time[(station[course[k][0]], station[course[k + 1][0]])]
+    return(cost)
 
-def pause(course):
-    #renvoie la liste des temps de pause intermediare pour un ensemble de course. Listepause[k] est la pause entre le keme et le k+1eme element de course
-    listepause=[]
+def compute_slack_periods(course):
+    """
+        Return the list of slack periods for a set of bookings.
+        slack[k] is the slack time between the k-th and de k+1-th stops 
+    """
+    slack = []
     for k in range(len(course) - 1):
-        listepause.append(course[k + 1][1] - time[(station[course[k][0]], station[course[k + 1][0]])] - duree[course[k][0]] - course[k][1])
-    return(listepause)
+        slack.append(course[k + 1][1] - time[(station[course[k][0]], station[course[k + 1][0]])] - duration[course[k][0]] - course[k][1])
+    return slack
 
 
-def insertclient(client,voiture):
-    #renvoie la liste de toutes les ensemble de courses possible en inserant client dans  voiture
-    listeprecedentinsertion=[[],[]]
+def insert_client(client, shift):
+    """
+        Returns the list of all possible insertions of client in shift
+    """
+    previous_insertions = [[],[]]
     i=0
-    for job in [prise,depose]:
-        for k in range(1,len(res[voiture])):
-            #print(res[voiture][k][1])
-            if res[voiture][k][1]>=debut[job[client]]:
-                listeprecedentinsertion[i].append(k-1)
-            if res[voiture][k][1]>fin[job[client]]:
+    for job in [pick_up, drop_off] :
+        # Compare the job time window with the stops already planned (apart from the start depot)
+        for k in range(1, len(shift_schedules[shift])) :
+            if shift_schedules[shift][k][1] >= start[job[client]] :
+                previous_insertions[i].append(k-1)
+            if shift_schedules[shift][k][1] > end[job[client]] :
                 break
         i+=1
-    listecandidat=[]
 
-    for i in listeprecedentinsertion[0]:
-        for j in listeprecedentinsertion[1]:
-            candidat=[]
-            for x in res[voiture]:
+    candidates = []
+
+    for i in previous_insertions[0]:
+        for j in previous_insertions[1]:
+            candidat = []
+            for x in shift_schedules[shift]:
                 candidat.append(x[:])
 
-            listepause=pause(candidat)
-            retard=time[(station[candidat[i][0]],station[prise[client]])]+time[(station[prise[client]],station[candidat[i+1][0]])]-time[(station[candidat[i][0]],station[candidat[i+1][0]])]+duree[prise[client]]
-            candidat=candidat[:i+1]+[[prise[client],max(debut[prise[client]],candidat[i][1]+time[(station[candidat[i][0]],station[prise[client]])]+duree[candidat[i][0]])]]+candidat[i+1:]
+            slack = compute_slack_periods(candidat)
 
+            delay = time[(station[candidat[i][0]], station[pick_up[client]])] \
+                + time[(station[pick_up[client]],station[candidat[i+1][0]])] \
+                - time[(station[candidat[i][0]],station[candidat[i+1][0]])] \
+                + duration[pick_up[client]]
+
+            candidat = candidat[:i+1] \
+                + [[pick_up[client], \
+                     max(start[pick_up[client]], \
+                         candidat[i][1] + time[(station[candidat[i][0]], station[pick_up[client]])] \
+                + duration[candidat[i][0]])]]+candidat[i+1:]
 
             for k in range(i+2,len(candidat)-1):
-                #print(retard,listepause[k-1])
-                retard=max(0,retard-listepause[k-1])
+                delay = max(0, delay - slack[k-1])
 
-                candidat[k][1]+=retard
+                candidat[k][1] += delay
 
-            listepause=pause(candidat)
-            retard=time[(station[candidat[j+1][0]],station[depose[client]])]+time[(station[depose[client]],station[candidat[j+2][0]])]-time[(station[candidat[j+1][0]],station[candidat[j+2][0]])]+duree[depose[client]]
+            slack = compute_slack_periods(candidat)
+            delay = time[(station[candidat[j+1][0]],station[drop_off[client]])] \
+                + time[(station[drop_off[client]], station[candidat[j+2][0]])] \
+                - time[(station[candidat[j+1][0]], station[candidat[j+2][0]])] \
+                + duration[drop_off[client]]
 
-            candidat=candidat[:j+2]+[[depose[client],max(debut[depose[client]],candidat[j+1][1]+time[(station[candidat[j+1][0]],station[depose[client]])]+duree[candidat[j+1][0]])]]+candidat[j+2:]
+            candidat = candidat[:j+2] + \
+                [[drop_off[client], \
+                    max(start[drop_off[client]], \
+                        candidat[j+1][1] + time[(station[candidat[j+1][0]], station[drop_off[client]])] \
+                + duration[candidat[j+1][0]])]] + candidat[j+2:]
 
             for k in range(j+3,len(candidat)-1):
-                #print(retard,listepause[k-1])
-                retard=max(0,retard-listepause[k-1])
+                delay = max(0,delay - slack[k-1])
 
-                candidat[k][1]+=retard
+                candidat[k][1] += delay
 
-            if verifie(candidat,voiture):
-                listecandidat.append([cout(candidat),candidat])
-    return(listecandidat)
+            if check_constraints(course = candidat, shift = shift):
+                candidates.append([cost(candidat), candidat])
 
-def insertclientoptimal(client,voiture):
-    #trouve la façon optimal d'inserer  client dans voiture
-    listecandidat=insertclient(client,voiture)
+    return(candidates)
 
-    if len(listecandidat)>0:
-        best_course=min(listecandidat)
-        return(best_course)
+
+def insert_optimal_client(client, shift):
+    """
+        Find the optimal way to insert client in shift
+    """
+
+    candidates = insert_client(client = client, shift = shift)
+
+    if len(candidates) > 0 :
+        best_candidate = min(candidates)
+        return(best_candidate)
     else:
         return None
 
-# on insere les clients par ordre croissant d'arrivée
-res={}
-for voiture in capacite.keys():
-    res[voiture]=[[prise[voiture],debut[prise[voiture]]],[depose[voiture],fin[depose[voiture]]]]
 
-listeclient=[]
-clientrejete=[]
+if __name__ == '__main__' : 
 
-for client in dureemax.keys():
-    listeclient.append((debut[depose[client]],client))
-listeclient.sort()
+    # shift_schedules[shift] represents the set of stops in the schedule of a shift
+    shift_schedules = {}
+    for shift in capacity.keys():
+        shift_schedules[shift]=[[pick_up[shift],start[pick_up[shift]]],[drop_off[shift],end[drop_off[shift]]]]
 
-for client in listeclient:
-    listecandidat=[]
-    for voiture in salairemax.keys():
-        course=insertclientoptimal(client[1], voiture)
-        if course!=None:
-            listecandidat.append([course[0] - cout(res[voiture]), course[1], voiture])
-    if len(listecandidat)>0:
-        best=min(listecandidat)
-        res[best[2]]=best[1]
-    else:
-        l=clientrejete.append(client)
+    unassigned_clients = []
 
+    # Sort each booking (client) according to their earliest drop off lower bound time window
+    sorted_clients = []
+    for client in max_ride_time.keys():
+        sorted_clients.append((start[drop_off[client]],client))
+    sorted_clients.sort()
 
-#ecriture du json
-nb_assigned_bookings=len(listeclient)-len(clientrejete)
-shifts=[]
-route_cost=0
+    for client in sorted_clients:
 
-for voiture in res.keys():
-    route_cost+=cout(res[voiture])
-    dic={}
-    dic["id"]=voiture
-    jobs=[]
-    for job in res[voiture]:
-        jobs.append({"id":job[0],"time":job[1]})
-    dic["jobs"]=jobs
-    shifts.append(dic)
+        feasible_insertions = []
+
+        for shift in max_turnover.keys():
+            # Find optimal insertion of client in shift
+            course = insert_optimal_client(client = client[1], shift = shift)
+            if course != None:
+                feasible_insertions.append([course[0] - cost(shift_schedules[shift]), course[1], shift])
+
+        # Check whether at least an insertion in whatever shift is possible
+        if len(feasible_insertions) > 0:
+            optimal_insertion = min(feasible_insertions)
+            shift_schedules[optimal_insertion[2]] = optimal_insertion[1]
+        else:
+            l = unassigned_clients.append(client)
 
 
-resjson={"nb_assigned_bookings":nb_assigned_bookings,"route_cost":route_cost,"shifts":shifts}
-with open('insertion_heuristics/results/results_week_insertion.json', 'w', encoding='utf-8') as f:
-    json.dump(resjson, f, indent=4)
+    # Json writing
+    nb_assigned_bookings = len(sorted_clients) - len(unassigned_clients)
+    shifts = []
+    route_cost = 0
+
+    for shift in shift_schedules.keys():
+        route_cost += cost(shift_schedules[shift])
+        dic = {}
+        dic["id"] = shift
+        jobs = []
+        for job in shift_schedules[shift]:
+            jobs.append({"id":job[0],"time":job[1]})
+        dic["jobs"]=jobs
+        shifts.append(dic)
+
+    resjson={"nb_assigned_bookings":nb_assigned_bookings, "route_cost":route_cost, "shifts":shifts}
+    with open('insertion_heuristics/results/results_week_insertion.json', 'w', encoding='utf-8') as f:
+        json.dump(resjson, f, indent=4)
 
 
